26f45ad5751bcef17239b551a82a302458dea46b
Lum1naT <47953544+Lum1naT@users.noreply.github.com>
2022-02-19 14:50:45 +0100

    database init

--- endevel_entry/endevel_entry/settings.py
+++ endevel_entry/endevel_entry/settings.py
@@ -11,10 +11,18 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+
+environ.Env.read_env(os.path.join(BASE_DIR, 'settings.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -73,10 +81,15 @@ WSGI_APPLICATION = 'endevel_entry.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': str(env("POSTGRES_DB")),
+        'USER': str(env("POSTGRES_USER")),
+        'PASSWORD': str(env("POSTGRES_PASSWORD")),
+        'HOST': str(env("POSTGRES_HOST")),
+        'PORT': '',
     }
 }
 
